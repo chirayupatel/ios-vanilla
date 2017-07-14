48b5621fd90a0f9f059a6b7a6111e7ae8ee6ee7b
Alex Smith <alex.smith@flybits.com>
2017-07-13 23:53:34 -0400

    Add RestaurantContextPlugin

---
+++ Vanilla/RestaurantContextPlugin.swift
@@ -0,0 +1,47 @@
+//
+//  RestaurantDataContextPlugin.swift
+//  Vanilla
+//
+//  Created by Alex on 7/12/17.
+//  Copyright © 2017 Alex. All rights reserved.
+//
+
+import FlybitsContextSDK
+
+// ProprietaryDataProvider allows to create and send proprietary custom context data for evaluating context rules.
+public class RestaurantDataContextPlugin: NSObject, ContextPlugin {
+    public var pluginID: String = "ctx.rgarestaurant.restaurant"
+    public var refreshTime: Int32 = 15
+    public var timeUnit: TimeUnit = .seconds
+    
+    // Initialize proprietary custom data
+    public var dietary: String
+    public var price: Double
+    public var calorie: Double
+    
+    public init(dietary: String = "", price: Double, calorie: Double = -Double.greatestFiniteMagnitude) {
+        self.dietary = dietary
+        self.price = price
+        self.calorie = calorie
+        super.init()
+    }
+    
+    public func refreshData(completion: @escaping (Any?, NSError?) -> Void) {
+        
+        // Build context data for rules evaluation
+        var customData = [String: Any]()
+        customData["dietary"] = dietary
+        customData["price"] = price
+        customData["calorie"] = calorie
+        
+        completion(customData, nil)
+    }
+    
+    public static func ==(lhs: RestaurantDataContextPlugin, rhs: RestaurantDataContextPlugin) -> Bool {
+        
+        if lhs.dietary == rhs.dietary && lhs.price == rhs.price && lhs.calorie == rhs.calorie {
+            return true
+        }
+        return false
+    }
+}
